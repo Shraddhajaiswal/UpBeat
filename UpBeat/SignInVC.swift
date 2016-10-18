446afa5b8d8d3c5da9eac0130cb916a6d379cf19
Shraddha Jaiswal <shraddhavuxva@gmail.com>
2016-10-19 00:24:17 +0530

    sign in layout complete

--- UpBeat/SignInVC.swift
+++ UpBeat/SignInVC.swift
@@ -7,19 +7,57 @@
 //
 
 import UIKit
+import AVKit
+import AVFoundation
+
+
 
 class SignInVC: UIViewController {
+    
+    
+    @IBOutlet weak var logo: UILabel!
+    
+    var player: AVPlayer?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let vedioURL: URL = Bundle.main.url(forResource: "video", withExtension: "mp4")!
+        view.backgroundColor = UIColor(white: 0.0,alpha: 0.0)
+     
+        
+        
+       
+        player = AVPlayer(url: vedioURL)
+        player?.actionAtItemEnd = .none
+        player?.isMuted = true
+        
+        let playerLayer = AVPlayerLayer(player: player)
+        playerLayer.videoGravity = AVLayerVideoGravityResizeAspectFill
+        playerLayer.zPosition = -1
+        
+        playerLayer.frame = view.frame
+        
+        view.layer.addSublayer(playerLayer)
+        
+        player?.play()
+        
+        //loop video
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(SignInVC.loopVideo),
+                                               name: NSNotification.Name.AVPlayerItemDidPlayToEndTime,
+                                               object: nil)
+    }
+    
+    func loopVideo() {
+        player?.seek(to: kCMTimeZero)
+        player?.play()
+    }
+    
+
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
 
-}
 
